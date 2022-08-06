0612436efd141e47884d0ca21da88d1ad8f70694
dao edu <daolq241@gmail.com>
2022-08-06 17:13:37 +0700

    up

---
+++ src/main/java/com/example/cineflix/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.example.cineflix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+	 
+	@GetMapping("")
+	public ModelAndView home() {
+		ModelAndView andView = new ModelAndView("home");
+		
+		int soA = 10;
+		int soB = 20; 
+		int tong = tinhTong(soA, soB);
+		
+		andView.addObject("ketqua", tong);
+		
+		
+		
+		
+		return andView;
+	}
+	
+	private int tinhTong(int soA, int soB) {
+		int tong = 0;
+		tong = soA + soB;
+		return tong;
+	}
+}
